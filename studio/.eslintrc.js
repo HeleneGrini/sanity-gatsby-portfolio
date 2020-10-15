b911b9904d048f497ba3c9ed26e886cc1f1bfa48
Helene Grini <helenegrini96@gmail.com>
2020-10-15 07:16:47 +0200

    update eslint

--- studio/.eslintrc.js
+++ studio/.eslintrc.js
@@ -1,14 +1,16 @@
 module.exports = {
-  extends: ['standard', 'standard-react'],
-  parser: 'babel-eslint',
+  extends: ["standard", "standard-react"],
+  parser: "babel-eslint",
   rules: {
-    'react/prop-types': 0,
-    'object-curly-spacing': ['error', 'never']
+    "react/prop-types": 0,
+    quotes: ["error", "double", "single"],
+    semi: [2, "always"],
+    "space-before-function-paren": ["error", "never"]
   },
   settings: {
     react: {
-      pragma: 'React',
-      version: '16.8.6'
+      pragma: "React",
+      version: "16.8.6"
     }
   }
-}
+};
